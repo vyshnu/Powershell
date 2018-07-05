996fae73caa08358cfdd04d7533d6f06a0ed1d8c
Vyshnavi Tekulapalli <vyshu.tekulapalli@outlook.com>
2018-07-05 05:50:56 +0530

    Add files via upload

---
+++ EnablingServices.ps1
@@ -0,0 +1,17 @@
+﻿param(
+[Parameter(Mandatory=$true)]
+[string[]]$ssissvname,
+[Parameter(Mandatory=$true)]
+[string]$Startuptype
+)
+
+#-----------------------------Enabling Services----------------------------------------------------------------------
+$ssissvname=$ssissvname.Split(",")
+foreach($svc in $ssissvname)
+{
+$service = gwmi win32_service -filter "name='$svc'"
+Set-Service –Name $svc –StartupType $Startuptype
+$service.StartService()
+}
+
+#---------------------------------------------------------------------------------------------------------------
