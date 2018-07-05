76286176fe2790023286510155a00ecbaaa2ab2e
Vyshnavi Tekulapalli <vyshu.tekulapalli@outlook.com>
2018-07-05 05:54:01 +0530

    Add files via upload

---
+++ authentication.ps1
@@ -0,0 +1,22 @@
+﻿param(
+[string]$wname
+)
+$AppCmd = "$env:windir\system32\inetsrv\appcmd.exe" 
+& $AppCmd unlock config /section:anonymousAuthentication
+& $AppCmd set config $wname -section:system.webServer/security/authentication/anonymousAuthentication /enabled:"False"
+
+
+#Unlocking and disabling the BasicAuthentication
+& $AppCmd unlock config /section:basicAuthentication
+& $AppCmd set config $wname -section:system.webServer/security/authentication/basicAuthentication /enabled:"False"
+
+
+#Unlocking and disabling the DigestAuthentication
+& $AppCmd unlock config /section:digestAuthentication
+& $AppCmd set config $wname -section:system.webServer/security/authentication/digestAuthentication /enabled:"False"
+
+& $AppCmd unlock config /section:windowsAuthentication
+& $AppCmd set config $wname -section:system.webServer/security/authentication/windowsAuthentication /enabled:"True"
+
+
+
