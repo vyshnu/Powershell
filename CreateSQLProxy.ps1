2768c3211ae24c021e659d66871b8e6f283e396c
Vyshnavi Tekulapalli <vyshu.tekulapalli@outlook.com>
2018-07-05 05:45:26 +0530

    Add files via upload

---
+++ CreateSQLProxy.ps1
@@ -0,0 +1,24 @@
+﻿param(
+[Parameter(Mandatory=$true)]
+[string]$ProxyName,
+[Parameter(Mandatory=$true)]
+[string]$ProxyCredentialName
+)
+
+#-----------------------------------------SQL Server Proxy------------------------------------------------------------------
+
+[Reflection.Assembly]::LoadWithPartialName("Microsoft.SqlServer.SMO")
+$server = New-Object Microsoft.SqlServer.Management.Smo.Server '.'
+$jobserver = $server.JobServer
+$subsystemos = [Microsoft.SqlServer.Management.Smo.Agent.AgentSubSystem]::CmdExec
+$subsystemssis = [Microsoft.SqlServer.Management.Smo.Agent.AgentSubSystem]::SSIS
+if(!$JobServer.ProxyAccounts[$ProxyName])
+{
+    $proxy = New-Object Microsoft.SqlServer.Management.Smo.Agent.ProxyAccount($JobServer,$ProxyName,$ProxyCredentialName, $true, "Proxy")
+    $proxy.Create()
+    $proxy.AddSubSystem($subsystemssis)
+    $proxy.AddSubSystem($subsystemos)
+    $proxy.Alter()
+}
+
+#--------------------------------------------------------------------------------------------------------------------------
---
+++ CreateSSISCatalog.ps1
@@ -0,0 +1,29 @@
+﻿param(
+[Parameter(Mandatory=$true)]
+[string]$catalogpwd)
+#----------------------------creating ssisdb catalog------------------------------------------------
+try{
+# Load the IntegrationServices Assembly  
+[Reflection.Assembly]::LoadWithPartialName("Microsoft.SqlServer.Management.IntegrationServices")  
+
+# Store the IntegrationServices Assembly namespace to avoid typing it every time  
+$ISNamespace = "Microsoft.SqlServer.Management.IntegrationServices"  
+
+Write-Host "Connecting to server ..."  
+
+# Create a connection to the server  
+$sqlConnectionString = "Data Source=localhost;Initial Catalog=master;Integrated Security=SSPI;"  
+$sqlConnection = New-Object System.Data.SqlClient.SqlConnection $sqlConnectionString  
+
+# Create the Integration Services object  
+$integrationServices = New-Object $ISNamespace".IntegrationServices" $sqlConnection  
+
+# Provision a new SSIS Catalog  
+$catalog = New-Object $ISNamespace".Catalog" ($integrationServices, "SSISDB", $catalogpwd)  
+$catalog.Create()
+}
+catch
+{
+Write-Verbose "SSISDB catalog already created"
+}
+#-----------------------------------------------------------------------------------------------------------------------
