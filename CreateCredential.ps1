48305584e5aad65dce5c4e0ae2955b60a78cf0dd
Vyshnavi Tekulapalli <vyshu.tekulapalli@outlook.com>
2018-07-05 05:46:20 +0530

    Add files via upload

---
+++ CreateCredential.ps1
@@ -0,0 +1,10 @@
+﻿param(
+[string]$credentialname,
+[Parameter(Mandatory=$true)]
+[string]$credidentity,
+[Parameter(Mandatory=$true)]
+[string]$credpassword)
+#-----------------------------------------SQL Server Credential---------------------------------------------------------
+Invoke-SqlCmd -Query "CREATE CREDENTIAL $credentialname WITH IDENTITY = '$credidentity',SECRET = '$credpassword'" -ErrorAction SilentlyContinue
+
+#-------------------------------------------------------------------------------------------------------------------------
